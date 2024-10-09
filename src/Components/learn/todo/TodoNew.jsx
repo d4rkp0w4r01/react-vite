716087925f5d01969848186ff0964401d8de37f3
d4rkp0w4r01 <jackspider2002@gmail.com>
2024-10-09 22:14:55 +0700

    video 36

--- src/Components/learn/todo/TodoNew.jsx
+++ src/Components/learn/todo/TodoNew.jsx
@@ -1,12 +1,24 @@
 const TodoNew = (props) =>{
     console.log(`check point`,props);
     const {addNewTodo} = props;
-    addNewTodo(`suba`);
+    //addNewTodo(`suba`);
+    const handleClick = () => {
+        alert(`click me`);
+
+    }
+    const handleOnChange = (name) =>{
+        console.log(`>>> handleOnchange`,name);
+    }
     return(
         <div className='todo-new'>
-        <input className='input' type="text"/>
-        <button className='button'>Add</button>
+        <input className='input' type="text"
+        onChange={(event) => handleOnChange(event.target.value)}
+        />
+        <button className='button'
+        onClick={handleClick}
+        >Add</button>
         </div>
+    
     );
 }
 export default TodoNew;
