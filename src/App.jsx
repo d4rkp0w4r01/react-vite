cf2340c354080fe991c9293b22c5c3f99a390118
d4rkp0w4r01 <jackspider2002@gmail.com>
2024-10-09 21:04:00 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import TodoData from './Components/learn/todo/TodoData';
 import TodoNew from './Components/learn/todo/TodoNew';
 import ReactLogo from  './assets/react.svg';
 const App = () => {
+  const name = `bryan`
+  const age = 25
+  const data = {
+    address:`hanoi`,
+    country:`vietnam`
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
 
       <TodoNew />
-      <TodoData/>
+      <TodoData
+      name={name}
+      age={age}
+      data={data}/>
       <div className='todo-image'>
       <img src={ReactLogo} className='logo' />
       </div>
--- src/Components/learn/todo/TodoData.jsx
+++ src/Components/learn/todo/TodoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () =>{
+const TodoData = (props) =>{
+    const {name, age, data} = props; //cach code nay hay su dung
+    console.log(`check props: `, props)
     return(
       <div className='todo-data'>
+        <div>Hello my name is {name}</div>
       <div>Leaning Hacking</div>
       <div>Learning Pwnable</div>
       </div>
