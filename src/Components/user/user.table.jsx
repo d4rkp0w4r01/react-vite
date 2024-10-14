20fc441153ebc80bbc390c473bf19095903528f4
d4rkp0w4r01 <jackspider2002@gmail.com>
2024-10-14 19:09:03 +0700

    video 77

--- src/Components/user/user.table.jsx
+++ src/Components/user/user.table.jsx
@@ -1,9 +1,11 @@
 import { Space, Table, Tag } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
 const UserTable = (props) => {
     const { dataUsers } = props
-
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+    const [dataUpdate, setDataUpdate] = useState (null);
     const columns = [
         {
             title: 'ID',
@@ -35,7 +37,12 @@ const UserTable = (props) => {
             key: 'action',
             render: (_, record) => (
                 <Space size="middle">
-                    <EditOutlined />
+                    <EditOutlined 
+                    onClick={() => {
+                        setDataUpdate(record);
+                        setIsModalUpdateOpen(true);
+                    }}
+                    />
                     <DeleteOutlined />
                 </Space>
             ),
@@ -48,7 +55,12 @@ const UserTable = (props) => {
                 dataSource={dataUsers}
                 rowKey={`_id`}
             />
-        <UpdateUserModal/>
+        <UpdateUserModal
+        isModalUpdateOpen={isModalUpdateOpen}
+        setIsModalUpdateOpen={setIsModalUpdateOpen}
+        dataUpdate = {dataUpdate}
+        setDataUpdate = {setDataUpdate}
+        />
         </>
     );
 }
