6eae050f57db3d52717373bac9a94a6c17f11900
d4rkp0w4r01 <jackspider2002@gmail.com>
2024-10-15 22:46:44 +0700

    video 82

--- src/Components/user/view.user.detail.jsx
+++ src/Components/user/view.user.detail.jsx
@@ -1,5 +1,6 @@
 import { Drawer } from 'antd';
-
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, message, Upload } from 'antd';
 const ViewUserDetail = (props) => {
 
     const {
@@ -11,6 +12,7 @@ const ViewUserDetail = (props) => {
 
     return (
         <Drawer title="Chi tiết User"
+            width={`25vw`}
             onClose={() => {
                 setDataDetail(null);
                 setIsDetailOpen(false);
@@ -25,6 +27,29 @@ const ViewUserDetail = (props) => {
                 <p>Email: {dataDetail.email}</p>
                 <br />
                 <p>Phone number: {dataDetail.phone}</p>
+                <br />
+                <div>
+                    <center><img height={300} width={300}
+                        src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} /></center>
+                </div>
+                <br />
+                <div>
+                    <center>
+                        <label htmlFor='btnUpload' style={{
+                            display: "block", 
+                            width: "fit-content", 
+                            marginTop: "15px", 
+                            padding: "5px 10px", 
+                            background: "orange", 
+                            borderRadius: "5px", 
+                            cursor: "pointer"
+                        }}
+
+                        >Upload Avatar</label>
+                        <input type="file" hidden id='btnUpload' />
+                    </center>
+                </div>
+                {/* <Button icon={<UploadOutlined />}>Click to Upload</Button> */}
             </>
                 :
                 <>
