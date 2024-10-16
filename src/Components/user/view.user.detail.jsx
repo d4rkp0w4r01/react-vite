9fd8d71c23673875fb2776618084e0f55ed464a8
d4rkp0w4r01 <jackspider2002@gmail.com>
2024-10-16 10:13:18 +0700

    video 83

--- src/Components/user/view.user.detail.jsx
+++ src/Components/user/view.user.detail.jsx
@@ -1,6 +1,7 @@
 import { Drawer } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload } from 'antd';
+import { useState } from 'react';
 const ViewUserDetail = (props) => {
 
     const {
@@ -10,6 +11,23 @@ const ViewUserDetail = (props) => {
         setIsDetailOpen
     } = props;
 
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+    const handleOnChangeFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            return (null)
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file))
+        }
+        console.log(`check file`, preview)
+
+    }
     return (
         <Drawer title="Chi tiết User"
             width={`25vw`}
@@ -36,20 +54,29 @@ const ViewUserDetail = (props) => {
                 <div>
                     <center>
                         <label htmlFor='btnUpload' style={{
-                            display: "block", 
-                            width: "fit-content", 
-                            marginTop: "15px", 
-                            padding: "5px 10px", 
-                            background: "orange", 
-                            borderRadius: "5px", 
+                            display: "block",
+                            width: "fit-content",
+                            marginTop: "15px",
+                            padding: "5px 10px",
+                            background: "orange",
+                            borderRadius: "5px",
                             cursor: "pointer"
                         }}
 
                         >Upload Avatar</label>
-                        <input type="file" hidden id='btnUpload' />
+                        <input
+                            type="file" hidden id='btnUpload'
+                            onChange={handleOnChangeFile}
+                        />
                     </center>
                 </div>
-                {/* <Button icon={<UploadOutlined />}>Click to Upload</Button> */}
+                <br />
+                {preview &&
+                    <div>
+                        <center><img height={300} width={300}
+                            src={preview} /></center>
+                    </div>
+                }
             </>
                 :
                 <>
